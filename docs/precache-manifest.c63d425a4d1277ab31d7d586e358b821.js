b0451c238ca226d5edb2ff333c2b4540a4f56625
Leudy <leudi_rosario@hotmail.com>
2020-06-28 21:47:34 -0400

    adding docs folder

---
+++ docs/precache-manifest.c63d425a4d1277ab31d7d586e358b821.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df84b9ba2c7b73a94aa8ae9ba8634725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2600c7bec4089845199e",
+    "url": "/static/js/2.03ccb853.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.03ccb853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1415d11d50bad01ee73f",
+    "url": "/static/js/main.464c59c6.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
--- src/heroes/HeroeList.js
+++ src/heroes/HeroeList.js
@@ -1,17 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import ProtoTypes from "prop-types";
 import { getHeroesByPublisher } from "../repositories/HeroesRepository";
 import { HeroeCard } from "./HeroeCard";
 
 export const HeroeList = ({ publisher }) => {
   // console.log(publisher);
-  const _heroes = getHeroesByPublisher(publisher);
-  // useEffect(() => {
-  //   console.log("Heroe List is load");
-  //   return () => {
-  //     console.log("heroe list was unload");
-  //   };
-  // }, [publisher]);
+  // const _heroes = getHeroesByPublisher(publisher);
+  const _heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+
   return (
     <div className="card-columns animate__animated animate__bounceInUp">
       {_heroes.map((h) => (
